227c193f253e94a31224e6c9323d7c249aabf204
Anonymous25201 <91649899+Anonymous25201@users.noreply.github.com>
2026-04-29 16:18:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+        tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        sans: ['Inter', 'sans-serif'],
+                        serif: ['Playfair Display', 'serif'],
+                        logo: ['Montserrat', 'sans-serif'], // Added custom logo font
+                    },
+                    colors: {
+                        brand: {
+                            dark: '#0a0a0a',
+                            light: '#f4f4f5',
+                            gold: '#cda95e',
+                            goldHover: '#b5934e',
+                            surface: '#18181b',
+                            border: '#27272a',
+                            input: '#27272a'
+                        }
+                    }
+                }
+            }
+        }
